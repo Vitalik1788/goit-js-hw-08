eb5d52131c758ffe5d454638a4884421bbe51e5e
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-03-06 22:10:18 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
