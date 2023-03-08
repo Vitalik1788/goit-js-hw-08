f6048daf5a13e71720f8ec831f725497cac17711
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-03-08 17:52:47 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textArea: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onInputValue, 500));
+refs.form.addEventListener('submit', onSubmitForm);
+
+onAutoCompliteForm();
+
+function onInputValue(event) {
+  const email = refs.form.elements.email.value;
+  const message = refs.form.elements.message.value;
+
+  const inputValue = {
+    email,
+    message,
+  }
+
+  const userInputValue = JSON.stringify(inputValue);
+  localStorage.setItem(STORAGE_KEY, userInputValue);
+
+};
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+
+function onAutoCompliteForm() {
+  const valueOfLocaleStorage = localStorage.getItem(STORAGE_KEY);
+  const validValueOfLocalStorage = JSON.parse(valueOfLocaleStorage);
+
+  if (validValueOfLocalStorage) {
+    refs.input.value = validValueOfLocalStorage.email;
+    refs.textArea.value = validValueOfLocalStorage.message;
+  }
+};
 
